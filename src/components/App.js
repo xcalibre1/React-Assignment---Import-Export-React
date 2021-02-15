f378c31da4bac9dbbff77b6df848b1e85069d46d
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-15 15:14:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import '../styles/App.css';
 import Heading from "./Heading.js"
 import SubHeading from "./SubHeading.js"
 import SubmitButton from "./SubmitButton.js"
+import InputQuery from "./InputQuery.js"
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
   )
 }
